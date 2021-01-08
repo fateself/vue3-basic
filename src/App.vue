25c2721aa2faf7d6912aff1d08ac3a0b9af05e85
binbin <liuhuibin@infohold.com.cn>
2021-01-08 18:07:52 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <h1>{{ count }}</h1>
+  <button @click="increase">+1</button>
+  <!-- <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/> -->
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+// import HelloWorld from "./components/HelloWorld.vue";
+
+import { ref } from "vue";
+export default defineComponent({
+  name: "App",
+  setup() {
+    const count = ref(0);
+    const increase = () => {
+      console.log(count)
+      count.value++;
+    };
+    return {
+      count,
+      increase,
+    };
+  },
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
